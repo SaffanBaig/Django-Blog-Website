1de3e919bac1d0eed3c538ba7c62034df36cafcb
SaffanBaig <mirzasaffan492@gmail.com>
2019-05-17 14:29:48 +0500

    Added static and template files

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "posted_by":"Saffan",
+        "description":"Our first post",
+        "posted_date":"May-17-19",
+        "title":"first Post"
+    },
+    {
+        "posted_by":"Hassan",
+        "description":"Our second post",
+        "posted_date":"May-17-19",
+        "title":"Second Post"
+    }
+]
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    context = {
+        "posts":posts
+    }
+    return render(request,'mainpage/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    context = {
+        "title":"about"
+    }
+    return render(request,'mainpage/about.html', context)
